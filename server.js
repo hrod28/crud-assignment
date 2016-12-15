321975fef1ed8d883bead129a6a89049f7339476
Laura Baukol <laura.baukol@mac.com>
2016-12-15 08:58:34 -0700

    updated server.js

--- server.js
+++ server.js
@@ -7,18 +7,49 @@ app.disable('x-powered-by');
 const bodyParser = require('body-parser');
 const knex = require('knex');
 const morgan = require('morgan');
-switch (app.get('env')){
-  case 'development':
-  app.use(morgan('dev'));
-  break;
+switch (app.get('env')) {
+    case 'development':
+        app.use(morgan('dev'));
+        break;
 
-  default:
+    case 'production':
+        app.use(morgan('short'));
+        break;
+
+    default:
 
 }
 
-app.use(express.static('public'));
+app.use(bodyParser.json());
 
+const contacts = require('./routes/contacts');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use(express.static('./public'));
+
+app.use(contacts);
+
+app.use((_req, res) => {
+  res.sendStatus(404);
 });
+
+app.use((err, _req, res, _next) => {
+    if (err.output && err.output.statusCode) {
+        return res
+            .status(err.output.statusCode)
+            .set('Content-Type', 'text/plain')
+            .send(err.message);
+    }
+
+    console.error(err.stack);
+    res.sendStatus(500);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    if (app.get('env') !== 'test') {
+        console.log('Listening on port', port);
+    }
+});
+
+module.exports = app;
